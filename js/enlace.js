1d92d7eaf354d83d5e4e6b53d47e8e5519da6aa3
Luis Dominguez <luis.dominguez.isw@gmail.com>
2025-01-20 22:27:52 -0700

    Update enlace.js

--- js/enlace.js
+++ js/enlace.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   buttonCV.addEventListener("click", () => {
     const openCV = document.createElement("a");
-    openCV.href = "/assets/cv/curriculum.pdf";
+    openCV.href = "https://iarthurdev.github.io/portafolio/assets/cv/curriculum.pdf";
     openCV.target = "_blank";
     openCV.click();
   });
