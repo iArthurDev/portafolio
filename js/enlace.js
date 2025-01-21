cfbf831d1f117b21f99a72595ed90954803ee976
iArthurDev <luis.dominguez.isw@gmail.com>
2025-01-20 21:56:15 -0700

    correccion de ruta de cv

--- js/enlace.js
+++ js/enlace.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   buttonCV.addEventListener("click", () => {
     const openCV = document.createElement("a");
-    openCV.href = "/assets/cv/curriculum.pdf";
+    openCV.href = "assets/cv/curriculum.pdf";
     openCV.target = "_blank";
     openCV.click();
   });
