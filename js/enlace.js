1839cf7df29eb35bc159581bf784c268274fa7d2
iArthurDev <luis.dominguez.isw@gmail.com>
2025-01-20 21:22:28 -0700

    primer commit portafolio

---
+++ js/enlace.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const buttonLink = document.getElementById("btnLink");
+  const buttonCV = document.getElementById("button-cv");
+  const buttonproject1 = document.getElementById("btn-project1");
+  const buttonproject2 = document.getElementById("btn-project2");
+
+  buttonLink.addEventListener("click", () => {
+    window.location.href =
+      "https://www.linkedin.com/in/luis-arturo-dominguez-alatorre";
+  });
+
+  buttonCV.addEventListener("click", () => {
+    const openCV = document.createElement("a");
+    openCV.href = "/assets/cv/curriculum.pdf";
+    openCV.target = "_blank";
+    openCV.click();
+  });
+
+  buttonproject1.addEventListener("click", () => {
+    window.location.href =
+      "https://github.com/iArthurDev/proyectoGestorDeProyectos.git";
+  });
+
+  buttonproject2.addEventListener("click", () => {
+    window.location.href = "https://github.com/iArthurDev/app-presupuesto.git";
+  });
+});
